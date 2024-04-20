d42e81aae55252e6f4b8e236b2389831f3709f38
kimmycolicot <166376182+kimmycolicot@users.noreply.github.com>
2024-04-20 14:37:11 +0800

    Update chart.js

--- chart.js
+++ chart.js
@@ -1,28 +1,53 @@
 var ctx = document.getElementById('myChart').getContext('2d');
+var donutChart = document.getElementById('donutChart').getContext('2d');
 var myChart = new Chart(ctx, {
+    type: 'doughnut',
+    data: {
+        labels: ['IICS', 'IHPD', 'IOM', 'ION', 'IOE'],
+        datasets: [{
+            label: 'Institutions',
+            data: [750, 800, 450, 250, 200],
+            backgrounColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(255, 150, 200, 1)'
+            ],
+
+    
+        }]
+    },
+    options: {
+        responsive: true,
+    }
+});
+
+var donutChart = new Chart(donutChart, {
     type: 'bar',
     data: {
-        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+        labels: ['IICS', 'IHPD', 'IOM', 'ION', 'IOE'],
         datasets: [{
-            label: '# of Votes',
-            data: [12, 19, 3, 5, 2, 3],
+            label: 'Evaluation Rate',
+            data: [60, 50, 55, 70, 75],
             backgrounColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)'
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)'
             ],
             borderColor: [
                 'rgba(255, 99, 132, 1)',
                 'rgba(54, 162, 235, 1)',
                 'rgba(255, 206, 86, 1)',
                 'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
+                'rgba(153, 102, 255, 1)'
             ],
-            borderWidth: 1
+            borderWidth: 2
         }]
+    },
+    options: {
+        responsive: true,
     }
-})
+});
